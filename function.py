decaff5a8fc1f1081c47e396d94f9985f7fd270d
Melipnos Viper <55535967+rosinaga@users.noreply.github.com>
2019-09-27 14:58:21 +0200

    duh

---
+++ function.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+    return a+b
+
+
+    # Testing:
+if (sum(1, 2) == 3 and
+    sum(0, -100) == -100 and
+    sum(-1, -2) == -3):
+
+    # Comment if all cases fine:
+    print("It works!")
+else:
+    print("Something's wrong!")
