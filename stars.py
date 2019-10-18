127a7ee817697b1b43bf8c49835b4320b2c484b0
Melipnos Viper <55535967+rosinaga@users.noreply.github.com>
2019-10-18 14:49:04 +0200

    stars

--- stars.py
+++ stars.py
@@ -1,5 +1,14 @@
 def stars1(n):
-    for i in range(5)
-    print("*")
+    for i in range(5):
+        print("*")
+        return ""
 
-print(stars1(4))
+#print(stars1(5))
+
+def stars_in_line(n):
+    stars = ""
+    for i in range(5):
+        stars = stars + "*"
+    return stars
+
+print(stars_in_line(5))
